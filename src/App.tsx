928e95641b568e7c7585f4d652a3fe924b77c3c1
Carlos Martinez <curlosmart@gmail.com>
2022-01-28 21:34:31 -0500

    setup routes and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
 import './App.css';
+import { Box, useDisclosure } from "@chakra-ui/react"
+import Home from './pages/Home';
+import Checkout from './pages/Checkout';
+import ProductDetail from './pages/ProductDetail';
+import Category from './pages/Category';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Box minHeight="100vh">
+        <Routes>
+          <Route path="/category/:name" element={<Category />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
   );
 }
 
