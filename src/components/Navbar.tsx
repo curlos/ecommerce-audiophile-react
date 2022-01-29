51c896b1ecab22600d7de897f010049a3058476c
Carlos Martinez <curlosmart@gmail.com>
2022-01-28 22:57:27 -0500

    added categories links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,18 +6,21 @@ const Navbar = () => {
 
 
   return (
-    <Flex align="center" bgColor="#101010" textColor="#ffffff" px="60px" py="25px">
-      <Image src="/assets/shared/desktop/logo.svg" />
-      <Flex flexGrow="1" justify="center">
-        <HStack spacing="30px">
-          <Link as={ReactLink} to="/">HOME</Link>
-          <Link as={ReactLink} to="/category/headphones">HEADPHONES</Link>
-          <Link as={ReactLink} to="/category/speakers">SPEAKERS</Link>
-          <Link as={ReactLink} to="/category/earphones">EARPHONES</Link>
-        </HStack>
+    <Box bgColor="#101010" textColor="#ffffff">
+      <Flex align="center" mx="60px" py="20px" borderBottom="1px" borderColor="#979797">
+        <Image src="/assets/shared/desktop/logo.svg" />
+        <Flex flexGrow="1" justify="center">
+          <HStack spacing="30px">
+            <Link as={ReactLink} to="/">HOME</Link>
+            <Link as={ReactLink} to="/category/headphones">HEADPHONES</Link>
+            <Link as={ReactLink} to="/category/speakers">SPEAKERS</Link>
+            <Link as={ReactLink} to="/category/earphones">EARPHONES</Link>
+          </HStack>
+        </Flex>
+        <Image src="/assets/shared/desktop/icon-cart.svg" />
+
       </Flex>
-      <Image src="/assets/shared/desktop/icon-cart.svg" />
-    </Flex>
+    </Box>
   );
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,60 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import { Box, Text, Image, VStack, Button, Flex, Grid } from '@chakra-ui/react';
 
 const Home = () => {
   return (
-    <div>
+    <Box>
       <Navbar />
 
-      Home
-    </div>
+      <Box bgImage="/assets/home/desktop/image-hero.jpg" h="500px" w="100vw" maxW="100%" bgSize="contain" bgRepeat="none" textColor="#FFFFFF" px="60px" py="60px">
+        <VStack align="start" spacing="15px" maxW="400px">
+          <Text fontWeight="400" fontSize="14px" letterSpacing="10px">NEW PRODUCT</Text>
+          <Text fontWeight="bold" fontSize="56px">XX99 MARK II HEADPHONES</Text>
+          <Text fontWeight="medium" fontSize="15px">Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</Text>
+          <Button bgColor="#D87D4A" fontWeight="medium" fontSize="13px" px="25px" py="15px" borderRadius="0px">SEE PRODUCT</Button>
+        </VStack>
+      </Box>
+
+      <Grid templateColumns="repeat(3, 1fr)" mx="70px" my="50px" gap="20px">
+        <Box>
+          <Image src="/assets/shared/desktop/image-category-thumbnail-headphones.png" height="185px" mb="-120px" ml="70px" />
+
+          <Flex flexDirection="column" justify="flex-end" align="center" gap="15px" py="40px" w="350px" h="204px" bgColor="#F1F1F1">
+            <Text fontWeight="bold" fontSize="18px">HEADPHONES</Text>
+            <Flex align="center" gap="10px">
+              <Text>SHOP</Text>
+              <Image src="/assets/shared/desktop/icon-arrow-right.svg" h="10px" w="5px" />
+            </Flex>
+          </Flex>
+        </Box>
+
+        <Box>
+          <Image src="/assets/shared/desktop/image-category-thumbnail-speakers.png" height="185px" mb="-120px" ml="70px" />
+
+          <Flex flexDirection="column" justify="flex-end" align="center" gap="15px" py="40px" w="350px" h="204px" bgColor="#F1F1F1">
+            <Text fontWeight="bold" fontSize="18px">SPEAKERS</Text>
+            <Flex align="center" gap="10px">
+              <Text>SHOP</Text>
+              <Image src="/assets/shared/desktop/icon-arrow-right.svg" h="10px" w="5px" />
+            </Flex>
+          </Flex>
+        </Box>
+
+        <Box>
+          <Image src="/assets/shared/desktop/image-category-thumbnail-earphones.png" height="185px" mb="-120px" ml="70px" />
+
+          <Flex flexDirection="column" justify="flex-end" align="center" gap="15px" py="40px" w="350px" h="204px" bgColor="#F1F1F1">
+            <Text fontWeight="bold" fontSize="18px">EARPHONES</Text>
+            <Flex align="center" gap="10px">
+              <Text>SHOP</Text>
+              <Image src="/assets/shared/desktop/icon-arrow-right.svg" h="10px" w="5px" />
+            </Flex>
+          </Flex>
+        </Box>
+
+      </Grid>
+    </Box>
   );
 };
 
